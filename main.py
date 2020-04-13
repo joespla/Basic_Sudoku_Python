141e09db033aed94b82b4216a4e293153be83d97
Jorge Espinosa Lara <joespla2000@gmail.com>
2020-04-13 11:03:00 -0500

    Basic method added

---
+++ main.py
@@ -0,0 +1,13 @@
+def isSudokuValid(firstLine, secondLine, thirdLine, fourthLine, fifthLine, sixthLine, seventhLine, eighthline, ninthLine):
+    
+    return null
+
+one = input("First line sudoku: ")
+two = input("Second line sudoku: ")
+three = input("Third line sudoku: ")
+four = input("Fourth line sudoku: ")
+five = input("Fifth line sudoku: ")
+six = input("Sixth line sudoku: ")
+seven = input("Seventh line sudoku: ")
+eight = input("Eighth line sudoku: ")
+nine = input("Ninth line sudoku: ")
